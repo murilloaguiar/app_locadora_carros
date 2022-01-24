ab185375ce51d2660fdf79b0d0caa2ead63adc26
Murillo Aguiar <aguiarmurillo1@gmail.com>
2022-01-24 12:06:09 -0300

    Inserindo marcas pelo front end

--- resources/js/app.js
+++ resources/js/app.js
@@ -33,6 +33,9 @@ Vue.component('table-component', require('./components/Table.vue').default);
 
 Vue.component('card-component', require('./components/Card.vue').default);
 
+Vue.component('modal-component', require('./components/Modal.vue').default);
+
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -38,7 +38,7 @@
                 <!-- /Card de busca -->
 
                 <!-- Card de listagem -->
-                <card-component titulo="Busca de marcas">
+                <card-component titulo="Relação de marcas">
                     
                     <template v-slot:conteudo>
                         <table-component>
@@ -47,41 +47,100 @@
                     </template>
 
                     <template v-slot:rodape>
-                        <button type="button" class="btn btn-primary btn-sm float-right">
+                        <button type="button" class="btn btn-primary btn-sm float-right" data-bs-toggle="modal" data-bs-target="#modalMarca">
                         Adicionar
                         </button>
                     </template>
 
                 </card-component>
                 <!-- /Card de listagem -->
+
             </div>
         </div>
-
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Launch demo modal
-        </button>
 
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save</button>
-                    </div>
+        <modal-component id="modalMarca" titulo="Adicionar marca">
+            
+            <template v-slot:conteudo>
+
+                <div class="form-group">
+                    <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp" texto-ajuda="Informe o nome da marca">
+
+                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca"
+                        v-model="nomeMarca"/>
+
+                    </input-container-component>
+
+                    {{ nomeMarca }}
+
+                    <input-container-component titulo="Imagem" id="novaImagem" id-help="novoImagemHelp" texto-ajuda="Seleciona uma imagem PNG">
+
+                        <input type="file" class="form-control-file" id="inputNome" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem" @change="carregarImagem($event)"/>
+
+                    </input-container-component>
+                    {{arquivoImagem}}
                 </div>
-            </div>
-        </div>
+
+            </template>
+
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
+            </template>
+
+        </modal-component>
     </div>
 </template>
 
 <script>
+    export default{
+        computed:{
+            token(){
+                let token = document.cookie.split(';').find(indice =>{
+                    return indice.includes('token=')
+                })
+                token = token.split('=')[1]
+                token = 'Bearer ' + token
+                return token
+            }
+        },
+
+        data(){
+            return {
+                urlBase:'http://localhost:8000/api/v1/marca',
+                nomeMarca: '',
+                arquivoImagem: []
+            }
+        },
+        methods:{
+            carregarImagem(event){
+                this.arquivoImagem = event.target.files
+            },
+
+            salvar(){
+                console.log(this.nomeMarca, this.arquivoImagem)
+
+                let formData = new FormData()
+                formData.append('nome', this.nomeMarca)
+                formData.append('imagem', this.arquivoImagem[0])
+
+                let config = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'Accept':'application/json',
+                        'Authorization': this.token
+                    }
+                }
+
+                axios.post(this.urlBase, formData, config)
+                    .then(response =>{
+                        console.log(response)
+                    })
+                    .catch(errors => {
+                        console.log(errors)
+                    })
+            }
+        }
+    }
+
 </script>
